35503f724c4b756057a0bf7c39212eaa8427d52c
Elijah Hudley <ElijahHudley@users.noreply.github.com>
2017-11-17 18:49:07 -0600

    starting to save

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -4,7 +4,6 @@ import {
     Switch,
     Redirect
 } from 'react-router-dom';
-import NotificationSystem from 'react-notification-system';
 
 import Header from 'components/Header/Header';
 import Footer from 'components/Footer/Footer';
@@ -17,77 +16,14 @@ import appRoutes from 'routes/app.jsx';
 
 class App extends Component {
     constructor(props){
+      console.log('props', props)
         super(props);
         this.componentDidMount = this.componentDidMount.bind(this);
-        this.handleNotificationClick = this.handleNotificationClick.bind(this);
-        this.state = {
-            _notificationSystem: null
-        };
     }
-    handleNotificationClick(position){
-        var color = Math.floor((Math.random() * 4) + 1);
-        var level;
-        switch (color) {
-            case 1:
-                level = 'success';
-                break;
-            case 2:
-                level = 'warning';
-                break;
-            case 3:
-                level = 'error';
-                break;
-            case 4:
-                level = 'info';
-                break;
-            default:
-                break;
-        }
-        this.state._notificationSystem.addNotification({
-            title: (<span data-notify="icon" className="pe-7s-gift"></span>),
-            message: (
-                <div>
-                    Welcome to <b>Light Bootstrap Dashboard</b> - a beautiful freebie for every web developer.
-                </div>
-            ),
-            level: level,
-            position: position,
-            autoDismiss: 15,
-        });
-    }
+
     componentDidMount(){
-        this.setState({_notificationSystem: this.refs.notificationSystem});
-        var _notificationSystem = this.refs.notificationSystem;
-        var color = Math.floor((Math.random() * 4) + 1);
-        var level;
-        switch (color) {
-            case 1:
-                level = 'success';
-                break;
-            case 2:
-                level = 'warning';
-                break;
-            case 3:
-                level = 'error';
-                break;
-            case 4:
-                level = 'info';
-                break;
-            default:
-                break;
-        }
-        _notificationSystem.addNotification({
-            title: (<span data-notify="icon" className="pe-7s-gift"></span>),
-            message: (
-                <div>
-                    This is the Dungeons and Dragons character Creator
-                </div>
-            ),
-            level: level,
-            position: "tr",
-            autoDismiss: 15,
-        });
     }
+
     componentDidUpdate(e){
         if(window.innerWidth < 993 && e.history.location.pathname !== e.location.pathname && document.documentElement.className.indexOf('nav-open') !== -1){
             document.documentElement.classList.toggle('nav-open');
@@ -100,27 +36,13 @@ class App extends Component {
 
 
         return (
-
                 <div className="wrapper">
-                    <NotificationSystem ref="notificationSystem" style={style}/>
                     <Sidebar {...this.props} />
                     <div id="main-panel" className="main-panel" style={Background}>
                         <Header {...this.props}/>
                             <Switch>
                                 {
                                     appRoutes.map((prop,key) => {
-                                        if(prop.name === "Notifications")
-                                            return (
-                                                <Route
-                                                    path={prop.path}
-                                                    key={key}
-                                                    render={routeProps =>
-                                                       <prop.component
-                                                           {...routeProps}
-                                                           handleClick={this.handleNotificationClick}
-                                                       />}
-                                                />
-                                            );
 
                                         if(prop.redirect)
                                             return (
